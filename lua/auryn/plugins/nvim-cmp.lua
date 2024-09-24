b345475647b2949ec7bb5b1842794ec937dd76d0
Auryn Engel <auryn_engel@mckinsey.com>
2024-09-24 11:26:18 +0200

    add supermaven

--- lua/auryn/plugins/nvim-cmp.lua
+++ lua/auryn/plugins/nvim-cmp.lua
@@ -40,7 +40,8 @@ return {
 			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "copilot" },
+				-- { name = "copilot" },
+				{ name = "supermaven" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
